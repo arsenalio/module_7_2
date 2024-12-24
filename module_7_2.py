905b6f936f0858f0a9084505f3bab8b1fd419d01
arsenalio <arslanlev@gmail.com>
2024-12-24 10:01:24 +0300

    Update module_7_2.py

--- module_7_2.py
+++ module_7_2.py
@@ -1,20 +1,21 @@
+from pprint import pprint
+
 def custom_write(file_name, strings):
-    n = 0
-    slem = {}
-    for i in info:
-        file = open(file_name, 'a', encoding='utf-8')
-        tell = (file.tell())
-        n += 1
-        file.write(f'{i}\n')
-        file.close()
-        slem.update({(n, tell):i})
-    return slem
+    strings_positions = {}
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for i, string in enumerate(strings, 1):
+            position = file.tell()
+            file.write(string + '\n')
+            strings_positions[(i, position)] = string
+    return strings_positions
+    
 info = [
     'Text for tell.',
     'Используйте кодировку utf-8.',
     'Because there are 2 languages!',
     'Спасибо!'
     ]
+
 result = custom_write('test.txt', info)
 for slem in result.items():
     print(slem)
